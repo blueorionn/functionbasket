41bb02fb6acf0c0f0b58c07ee7aed6921beda4a5
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-07 06:36:28 +0000

    created home page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/content", "@nuxt/fonts"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/content",
+    "@nuxt/fonts",
+    "@nuxt/image",
+  ],
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
   },
