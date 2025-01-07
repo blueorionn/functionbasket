8bbd5938cbc279e30113c304a796ed1e9c30d9a7
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-07 03:58:58 +0000

    tailwindcss configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/content"],
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css'
+  }
 });
