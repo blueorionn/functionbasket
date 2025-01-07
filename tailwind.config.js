3327ac5f5888a16cebc0775de0c82883240f7d12
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-07 04:08:53 +0000

    add content path, extended theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    // all directories and extensions will correspond to your Nuxt config
+    "{srcDir}/components/**/*.{vue,js,jsx,mjs,ts,tsx}",
+    "{srcDir}/layouts/**/*.{vue,js,jsx,mjs,ts,tsx}",
+    "{srcDir}/pages/**/*.{vue,js,jsx,mjs,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "background-dark": "var(--default-dark)",
+        "foreground-dark": "var(--default-light)",
+        background: "var(--default-light)",
+        foreground: "var(--default-dark)",
+      },
+    },
   },
   plugins: [],
 };
