736c3bc41dd0b7b6c9662faee4f3c68a1c8720c8
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-01-10 13:49:34 +0000

    removed about link

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -5,21 +5,6 @@
         <span class="_header-font text-xl text-gray-200">fb</span>
       </NuxtLink>
       <nav class="flex justify-center items-center gap-2 md:gap-4">
-        <NuxtLink to="/about" title="About">
-          <span
-            class="hidden lg:inline-block font-semibold transition-all hover:text-gray-200"
-            >About</span
-          >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 -960 960 960"
-            class="lg:hidden h-4 w-4 fill-gray-400 transition-all hover:fill-gray-500"
-          >
-            <path
-              d="M480-400q33 0 56.5-23.5T560-480q0-33-23.5-56.5T480-560q-33 0-56.5 23.5T400-480q0 33 23.5 56.5T480-400ZM320-240h320v-23q0-24-13-44t-36-30q-26-11-53.5-17t-57.5-6q-30 0-57.5 6T369-337q-23 10-36 30t-13 44v23ZM720-80H240q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80Zm0-80v-446L526-800H240v640h480Zm-480 0v-640 640Z"
-            />
-          </svg>
-        </NuxtLink>
         <NuxtLink to="/blog" title="Blog">
           <span
             class="hidden lg:inline-block font-semibold transition-all hover:text-gray-200"
