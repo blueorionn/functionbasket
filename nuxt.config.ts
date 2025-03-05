df3d93f68be4da3538716bdaf724d1eda633c157
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-05 12:58:40 +0000

    configure prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ["@nuxtjs/tailwindcss"],
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
-  }
-})
+  },
+});
