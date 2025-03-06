70714d1561551f08e57eb2a52267ad880f835af4
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-06 12:04:17 +0000

    adde ip checking validity

--- pages/tools/ip-obfuscator.vue
+++ pages/tools/ip-obfuscator.vue
@@ -15,16 +15,23 @@
         </p>
       </section>
       <section class="py-8">
-        <div class="w-full flex justify-center items-center gap-2.5">
-          <input
-            type="text"
-            name="ip"
-            id="ip"
-            placeholder="127.0.0.1"
-            class="p-4 max-w-96 w-full rounded border border-gray-300 bg-gray-50 focus:ring-3 focus:ring-blue-300"
-          />
+        <div class="w-full flex justify-center items-start gap-2.5">
+          <div class="max-w-96 w-full">
+            <input
+              type="text"
+              name="ip"
+              id="ip"
+              placeholder="127.0.0.1"
+              v-model="text"
+              class="p-4 w-full rounded border border-gray-300 bg-gray-50 focus:ring-3 focus:ring-blue-300"
+            />
+            <p class="py-1 text-sm text-red-500" v-if="hasError">
+              Please enter a valid Ipv4 address
+            </p>
+          </div>
           <button
             type="button"
+            @click="convert"
             class="p-4 px-6 rounded text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300"
           >
             <span class="font-semibold text-gray-100 convert-button"
@@ -42,7 +49,7 @@
             <div
               class="relative w-full p-4 border rounded-sm bg-white border-gray-200"
             >
-              <span class="text-gray-700">::ffff:7f00:0001</span>
+              <span class="text-gray-700">{{ ipv6_short }}</span>
               <button type="button" class="absolute right-[5%] my-auto">
                 <span class="sr-only">Copy Button</span>
                 <Icon
@@ -64,9 +71,7 @@
             <div
               class="relative w-full p-4 border rounded-sm bg-white border-gray-200"
             >
-              <span class="text-gray-700"
-                >0000:0000:0000:0000:0000:ffff:7f00:0001</span
-              >
+              <span class="text-gray-700">{{ ipv6_long }}</span>
               <button type="button" class="absolute right-[5%] my-auto">
                 <span class="sr-only">Copy Button</span>
                 <Icon
@@ -88,7 +93,7 @@
             <div
               class="relative w-full p-4 border rounded-sm bg-white border-gray-200"
             >
-              <span class="text-gray-700">2130706433</span>
+              <span class="text-gray-700">{{ ip_int }}</span>
               <button type="button" class="absolute right-[5%] my-auto">
                 <span class="sr-only">Copy Button</span>
                 <Icon
@@ -108,7 +113,7 @@
             <div
               class="relative w-full p-4 border rounded-sm bg-white border-gray-200"
             >
-              <span class="text-gray-700">2130706433</span>
+              <span class="text-gray-700">{{ ip_hex }}</span>
               <button type="button" class="absolute right-[5%] my-auto">
                 <span class="sr-only">Copy Button</span>
                 <Icon
@@ -128,7 +133,7 @@
             <div
               class="relative w-full p-4 border rounded-sm bg-white border-gray-200"
             >
-              <span class="text-gray-700">0177.0000.0000.0001</span>
+              <span class="text-gray-700">{{ ip_oct }}</span>
               <button type="button" class="absolute right-[5%] my-auto">
                 <span class="sr-only">Copy Button</span>
                 <Icon
@@ -148,9 +153,7 @@
             <div
               class="relative w-full p-4 border rounded-sm bg-white border-gray-200"
             >
-              <span class="text-gray-700"
-                >11000000000000100000001000001000</span
-              >
+              <span class="text-gray-700">{{ ip_bin }}</span>
               <button type="button" class="absolute right-[5%] my-auto">
                 <span class="sr-only">Copy Button</span>
                 <Icon
@@ -172,9 +175,7 @@
             <div
               class="relative w-full p-4 border rounded-sm bg-white border-gray-200"
             >
-              <span class="text-gray-700"
-                >11000000.00000010.00000010.00001000</span
-              >
+              <span class="text-gray-700">{{ ip_dot_bin }}</span>
               <button type="button" class="absolute right-[5%] my-auto">
                 <span class="sr-only">Copy Button</span>
                 <Icon
@@ -216,4 +217,48 @@ useSeoMeta({
     "An IP obfuscator is a tool used to mask, hide, or manipulate an IP address to make it difficult to trace the original source. It is commonly used in cybersecurity, privacy protection, and ethical hacking to anonymize network activity.",
   twitterImage: "https://www.functionbasket.com/favicon.ico",
 });
+
+// Conversion Logic
+const text = ref("");
+const hasError = ref(false);
+
+// Conversion formats
+const ipv6_short = ref("::ffff:7f00:0001");
+const ipv6_long = ref("0000:0000:0000:0000:0000:ffff:7f00:0001");
+const ip_int = ref("2130706433");
+const ip_hex = ref("0XC0.0X2.0X2.0X8");
+const ip_oct = ref("0177.0000.0000.0001");
+const ip_bin = ref("11000000000000100000001000001000");
+const ip_dot_bin = ref("11000000.00000010.00000010.00001000");
+
+function convert() {
+  checkValidity(text.value);
+}
+
+// Check ipv4 validity
+function checkValidity(ip: string) {
+  // reseting hasError variable
+  hasError.value = false;
+
+  // valid ipv4 has 4 octets
+  if (ip.split(".").length !== 4) {
+    hasError.value = true;
+  }
+
+  // checking range of octet
+  const octets = ip.split(".");
+  octets.forEach((octet) => {
+    try {
+      const intOctet = parseInt(octet);
+
+      if (intOctet < 255 || intOctet > 0) {
+        hasError.value = false;
+      } else {
+        hasError.value = true;
+      }
+    } catch (error) {
+      hasError.value = true;
+    }
+  });
+}
 </script>
