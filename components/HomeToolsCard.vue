9b02f69dd945cc346902b537c77f2fd58e12357b
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-06 10:46:40 +0000

    modified card title style

--- components/HomeToolsCard.vue
+++ components/HomeToolsCard.vue
@@ -3,7 +3,9 @@
     :href="toolUrl"
     class="group rounded shadow inline-block h-full w-full p-4 hover:shadow-md transition-all duration-200"
   >
-    <h2 class="py-2.5 font-bold text-lg group-hover:underline text-gray-800">
+    <h2
+      class="py-2.5 font-bold text-lg group-hover:underline text-gray-800 home-tools-card-title"
+    >
       {{ title }}
     </h2>
     <p class="py-0.5 font-normal text-base group-hover:underline text-gray-700">
@@ -13,7 +15,7 @@
 </template>
 
 <style>
-h2 {
+.home-tools-card-title {
   font-family: "Space Grotesk", sans-serif;
 }
 </style>
