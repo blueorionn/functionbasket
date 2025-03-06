38f72229d1d0d8ae54d00a6c6d849097cea255fd
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-03-06 11:32:53 +0000

    converted content url to relative

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,9 @@
 export default {
   content: [
     // all directories and extensions will correspond to your Nuxt config
-    "functionbasket/components/**/*.{vue,js,jsx,mjs,ts,tsx}",
-    "functionbasket/layouts/**/*.{vue,js,jsx,mjs,ts,tsx}",
-    "functionbasket/pages/**/*.{vue,js,jsx,mjs,ts,tsx}",
+    "./components/**/*.{vue,js,jsx,mjs,ts,tsx}",
+    "./layouts/**/*.{vue,js,jsx,mjs,ts,tsx}",
+    "./pages/**/*.{vue,js,jsx,mjs,ts,tsx}",
   ],
   theme: {
     extend: {
