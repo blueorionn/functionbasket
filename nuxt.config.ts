be0bf4ab94564e032cf6b6e9f7f7aca25cf36c90
Swadhin Tandi <168361244+blueorionn@users.noreply.github.com>
2025-04-01 10:15:15 +0000

    Transition to PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,45 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@nuxt/fonts", "@nuxt/image"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "@vite-pwa/nuxt",
+  ],
   tailwindcss: {
     cssPath: "~/assets/css/tailwind.css",
+  },
+  pwa: {
+    manifest: {
+      name: "Function Basket",
+      short_name: "FB",
+      theme_color: "#242124",
+      display: "standalone",
+      start_url: "/",
+      icons: [
+        {
+          src: "/favicon-16x16.png",
+          sizes: "16x16",
+          type: "image/png",
+        },
+        {
+          src: "/favicon-32x32.png",
+          sizes: "32x32",
+          type: "image/png",
+        },
+        {
+          src: "/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
   },
 });
