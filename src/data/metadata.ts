496186c4d13a864bed8ce0e421623dcc977f5c12
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-04-08 18:54:20 +0000

    configured prettier

--- src/data/metadata.ts
+++ src/data/metadata.ts
@@ -1,5 +1,6 @@
 export const MetaData = {
-    title: "Function Basket - Personal Toolkit Collection",
-    description: "This website is a personal toolkit designed to streamline various tasks for ethical hacking, networking, and general utility. It hosts a collection of tools I frequently use.",
-    faviconImg: "https://www.functionbasket.com/favicon.ico"
+  title: 'Function Basket - Personal Toolkit Collection',
+  description:
+    'This website is a personal toolkit designed to streamline various tasks for ethical hacking, networking, and general utility. It hosts a collection of tools I frequently use.',
+  faviconImg: 'https://www.functionbasket.com/favicon.ico',
 }
