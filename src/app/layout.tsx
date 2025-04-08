87a4013a0ae2e527f374de25e2b109b3d5e8f088
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-04-08 18:51:27 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { MetaData } from "@/data/metadata";
+import "@/styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: MetaData.title,
+  description: MetaData.description,
+  openGraph: {
+    title: MetaData.title,
+    description: MetaData.description,
+  },
+  twitter: {
+    card: "summary",
+    title: MetaData.title,
+    description: MetaData.description,
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/data/metadata.ts
@@ -0,0 +1,5 @@
+export const MetaData = {
+    title: "Function Basket - Personal Toolkit Collection",
+    description: "This website is a personal toolkit designed to streamline various tasks for ethical hacking, networking, and general utility. It hosts a collection of tools I frequently use.",
+    faviconImg: "https://www.functionbasket.com/favicon.ico"
+}
