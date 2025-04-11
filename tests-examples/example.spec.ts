81e186d284bd162295b6ce38fbbfdb009a4bbd1f
Swadhin Tandi <168361244+blueorionn@users.noreply.github.com>
2025-04-11 05:03:50 +0000

    chore: migrated example.spec.ts to tests folder

--- tests-examples/example.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('Home Page', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
-  await expect(page.locator('body')).toBeDefined()
-})
