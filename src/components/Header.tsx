b06c6a80d092472ebc7aa28e7e710d3aee7e567e
Swadhin <168361244+blueorionn@users.noreply.github.com>
2025-04-16 09:55:19 +0000

    fix: svg invisible

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -69,7 +69,7 @@ export default function Header() {
                 <DropdownMenuTrigger asChild>
                   <Button variant='outline' size='icon'>
                     <Sun className='h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
-                    <Moon className='absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
+                    <Moon className='absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 fill-gray-100 transition-all dark:scale-100 dark:rotate-0' />
                     <span className='sr-only'>Toggle theme</span>
                   </Button>
                 </DropdownMenuTrigger>
